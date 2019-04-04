0b5d41b7efbb029bd3ed7e84a293dc5d4c505316
akforceda <46069053+akforceda@users.noreply.github.com>
2019-04-04 20:27:26 +0530

    Update BubbleSort.java

--- sorting-algorithms-Java/src/com/akshaydand/main/BubbleSort.java
+++ sorting-algorithms-Java/src/com/akshaydand/main/BubbleSort.java
@@ -9,7 +9,8 @@ public class BubbleSort {
 		for (int i = inputArray.length - 1; i > 0; i--) {
 			for (int j = 0; j < i; j++) {
 				if (inputArray[j] > inputArray[j + 1]) {
-
+					
+					//swapping logic
 					int temp = inputArray[j];
 					inputArray[j] = inputArray[j + 1];
 					inputArray[j + 1] = temp;
@@ -17,6 +18,7 @@ public class BubbleSort {
 				}
 			}
 		}
+		//returning the sorted array
 		return inputArray;
 	}
 
@@ -25,16 +27,20 @@ public class BubbleSort {
 
 		int numberOfElements = Integer.parseInt(br.readLine());
 		int[] inputArray = new int[numberOfElements];
-
+		
+		//creating a string array after taking input in inputLine because, input is in a single line :5 8 9 6 3 41
 		String inputLine = br.readLine();
 		String[] stringInput = inputLine.trim().split("\\s+");
 
+		//Storing the elements to an Integer array using wrapper classes
 		for (int i = 0; i < numberOfElements; i++) {
 			inputArray[i] = Integer.parseInt(stringInput[i]);
 		}
 
+		//Storing the entire array returned from the sort method
 		int[] sortedArray = sort(inputArray);
 
+		//Displaying the sorted output
 		for (int i = 0; i < numberOfElements; i++) {
 			System.out.print(sortedArray[i] + " ");
 		}
